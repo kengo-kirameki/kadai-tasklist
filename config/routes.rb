343dfc05152f34ca0a953d0be6812ae91ef86f51
kengo.kirameki <xianwutianzhong@gmail.com>
2019-03-27 08:31:40 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
+  get 'login', to: 'session#new'
   
+  post 'login', to: 'session#create'
+  
+  delete 'logout', to: 'session#destroy'
+   
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root to: 'toppages#index'
+  
+  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:index,:show,:new,:create]
+  
 end
