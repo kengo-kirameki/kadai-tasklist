e6a827913ceabf66e79906521332599ab3893747
kengo.kirameki <xianwutianzhong@gmail.com>
2019-03-31 01:37:35 +0000

    post3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task,only:[:show,:edit,:update,:destroy]
-  before_action :correct_user,only:[:destroy]
+  before_action :correct_user,only:[:show,:destroy]
   
   def index
     @tasks = Task.all
@@ -61,9 +61,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id:params[:id])
     unless @task
-      redirect_to root-url
+      redirect_to root_url
     end
   end    
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
-   get 'login', to: 'session#new'
+  get 'login', to: 'session#new'
   
   post 'login', to: 'session#create'
   
   delete 'logout', to: 'session#destroy'
    
- 
   root to: 'toppages#index'
   
-  
   get 'signup', to: 'users#new'
-  resources :tasks
+  
+  resources :tasks, only: [:show,:new,:create,:edit,:update,:destroy]
   resources :users, only: [:index,:show,:new,:create]
   
 end
